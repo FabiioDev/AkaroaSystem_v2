3d28f691d2344ae6f366206fde2bcc6a628354e8
FabiioDev <faboordo@gmail.com>
2025-03-07 22:32:43 -0600

    Configuracion final

--- src/main/java/com/example/akaroa/AkaroaApplication.java
+++ src/main/java/com/example/akaroa/AkaroaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AkaroaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AkaroaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AkaroaApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/akaroa/AkaroaApplicationTests.java
+++ src/test/java/com/example/akaroa/AkaroaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AkaroaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
