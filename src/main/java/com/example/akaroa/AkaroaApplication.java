9becf7c24300aa331b4413f5fbb002835327e871
FabiioDev <faboordo@gmail.com>
2025-03-07 22:30:39 -0600

    Configuracion proyecto y datasource PostgreSQL

---
+++ src/main/java/com/example/akaroa/AkaroaApplication.java
@@ -0,0 +1,13 @@
+package com.example.akaroa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkaroaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkaroaApplication.class, args);
+	}
+
+}
